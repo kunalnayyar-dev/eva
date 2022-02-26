9c7c5c3bb471494586aa5c1838468a3de5f9a4d3
Kunal Nayyar <kunalnayyar28@gmail.com>
2022-02-26 12:09:03 +1100

    Mobile and Desktop CSS bugs resolved

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -9,10 +9,9 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="stylesheet" href="{{ asset('css/style.css?ver='.time()) }}">
-
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -81,20 +80,20 @@
         </div>
 
         <div class="offer-container mt-5">
-            <div class="col-lg-3 offer-content">
+            <div class="col-lg-3 offer-content flex-order">
                 <h3 class="primary-color">Hideaway Coffee Table</h3>
                 <p class="primary-color">Made to last and with plenty of storage, our square and long table are heroes of any living space.</p>
                 <button class="primary-button primary-bg-color">shop now</button>
             </div>
-            <div class="col-lg-3 offer-image">
+            <div class="col-lg-3 offer-image flex-order">
                 <img src="images/1.jpg">
             </div>
-            <div class="col-lg-3 offer-content">
+            <div class="col-lg-3 offer-content flex-order">
                 <h3 class="primary-color">Hideaway Side Table</h3>
                 <p class="primary-color">Made to last and with plenty of storage, the Hideaway Side Table perfectly complements your bed frame or sofa.</p>
                 <button class="primary-button primary-bg-color">shop now</button>
             </div>
-            <div class="col-lg-3 offer-image">
+            <div class="col-lg-3 offer-image flex-order">
                 <img src="images/2.jpg">
             </div>
         </div>
@@ -170,16 +169,10 @@
 
 
 
-<!-- Optional JavaScript; choose one of the two! -->
-
 <!-- Option 1: Bootstrap Bundle with Popper -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+{{--<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>--}}
 
-<!-- Option 2: Separate Popper and Bootstrap JS -->
-<!--
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
--->
 </body>
 </html>
 
